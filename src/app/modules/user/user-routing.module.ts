e869e8d9418298200d7db1878a36fff0d8dc1711
useyourillusions <minsk.bay@gmail.com>
2020-07-10 08:57:10 +0300

    Added user container component

--- src/app/modules/user/user-routing.module.ts
+++ src/app/modules/user/user-routing.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserContainerComponent } from './containers/user-container/user-container.component';
 import { MeComponent } from './components/me/me.component';
 import { EventsComponent } from './components/events/events.component';
-
 
 const routes: Routes = [
   {
     path: '',
+    component: UserContainerComponent,
     children: [
       {
         path: '',
         pathMatch: 'full',
+        redirectTo: 'me'
+      },
+      {
+        path: 'me',
         component: MeComponent
       },
       {
