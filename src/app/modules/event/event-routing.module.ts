9e97a5248f57a5e614145f4c8e7e749195a8b467
Dzmitry Mukaseyeu <dimamukas@gmail.com>
2020-07-13 23:02:54 +0300

    aded auth module

--- src/app/modules/event/event-routing.module.ts
+++ src/app/modules/event/event-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { EventComponent } from './components/event/event.component'
+import { EventComponent } from './components/event/event.component';
 
 
 const routes: Routes = [
