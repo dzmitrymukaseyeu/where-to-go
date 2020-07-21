b8a33a0549466f978b02980b65f18a914fbe6751
Dzmitry Mukaseyeu <dimamukas@gmail.com>
2020-07-21 12:54:18 +0300

    added filter for events

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { EventsAll } from '@app/shared/mocks';
 import { EventsAllDefinition } from '@app/shared/interfaces';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -9,13 +10,19 @@ import { EventsAllDefinition } from '@app/shared/interfaces';
 })
 export class HomeComponent implements OnInit {
   
-  EventsAll : EventsAllDefinition[]  = EventsAll;
-
+  EventsAll : EventsAllDefinition[] = EventsAll;
+  filteredEvents = new BehaviorSubject(this.EventsAll);
   constructor() { }
 
   ngOnInit(): void {
-
+ 
     console.log(EventsAll);
+  }
 
+  OnFilterClick(event) {
+    console.log(event);
+    const filteredEvents = this.EventsAll.filter(item => item.type === event.srcElement.innerText); 
+
+    this.filteredEvents.next(filteredEvents);
   }
 }
