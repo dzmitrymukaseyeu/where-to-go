25a6eb030382ddac472ee3a86db5405108de53c5
useyourillusions <minsk.bay@gmail.com>
2020-07-24 18:08:31 +0300

    Fixes for the new event form

--- src/app/modules/event/components/event/event.component.ts
+++ src/app/modules/event/components/event/event.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '@app/services';
-import { FormGroup, FormBuilder, Validator, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-event',
@@ -9,15 +9,17 @@ import { FormGroup, FormBuilder, Validator, Validators } from '@angular/forms';
 })
 export class EventComponent implements OnInit {
   imagesTable={
-    'Кино': "/assets/cinema.jpg",
-    'Концерты': "/assets/concert.jpg",
-    'Фестивали': "/assets/festival.jpg",
-    'Вечеринки': "/assets/party.jpg",
-    'Спектакли': "/assets/show.jpg",
-    'Выставки': "/assets/insert.jpg",
-    'Другое': "/assets/other.jpg",
-    'Активности': "/assets/active.jpg"
-  } 
+    'Кино': 'assets/cinema.jpg',
+    'Концерты': '/assets/concert.jpg',
+    'Фестивали': '/assets/festival.jpg',
+    'Вечеринки': '/assets/party.jpg',
+    'Спектакли': '/assets/show.jpg',
+    'Выставки': '/assets/insert.jpg',
+    'Другое': '/assets/other.jpg',
+    'Активности': '/assets/active.jpg'
+  };
+  eventsTypeList: string[] = Object.keys(this.imagesTable);
+
   creationEventForm: FormGroup;
 
   constructor(
@@ -27,12 +29,28 @@ export class EventComponent implements OnInit {
 
   ngOnInit(): void {
     this.creationEventForm = this.formBuilder.group({
-      title: [null,[Validators.required,Validators.minLength(10),Validators.maxLength(30)]],
-      description: [null,[Validators.required,Validators.minLength(20),Validators.maxLength(100)]],
-      date:[null,[Validators.required]],
-      time: [null,[Validators.required]],
-     //type:[null],
+      title: [null, [
+        Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(30)
+      ]],
+      description: [null, [
+          Validators.required,
+          Validators.minLength(20),
+          Validators.maxLength(100)
+      ]],
+      
+      date:[null, [
+        Validators.required
+      ]],
+      
+      time: [null, [
+        Validators.required
+      ]],
+
+      type:[this.eventsTypeList[0]],
     })
+
     // const user = {
     //     firstName: 'Коля',
     //     lastName: 'Лукашов',
