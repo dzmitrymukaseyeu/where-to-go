3b628494adeb7d686257dc81761617cf46dc2005
EdwardHushaleu <2779522@mail.com>
2020-07-26 00:36:49 +0300

    works with background and styls

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -29,6 +29,17 @@ export class HomeComponent implements OnInit {
     'Активности': "#4AA329",
   }
 
+  imagesTable={
+    'Кино': 'url(/assets/cinema.jpg)',
+    'Концерты': 'url(/assets/concert.jpg)',
+    'Фестивали': 'url(/assets/festival.jpg)',
+    'Вечеринки': 'url(/assets/party.jpg)',
+    'Спектакли': 'url(/assets/show.jpg)',
+    'Выставки': 'url(/assets/insert.jpg)',
+    'Другое': 'url(/assets/other.jpg)',
+    'Активности': 'url(/assets/active.jpg)'
+  };
+
   ngOnInit(): void {
 
     this.apiService.getAllEvents()
@@ -39,7 +50,7 @@ export class HomeComponent implements OnInit {
   OnFilterClick(event) {
     console.log(event);
     const filteredEvents = this.EventsAll.filter(item => item.type === event.srcElement.innerText); 
-
+    
     this.filteredEvents.next(filteredEvents);
   }
 }
