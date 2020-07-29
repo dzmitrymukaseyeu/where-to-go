8c6d025fa7f9fc3bfd38954eddcfa30d72fae2f1
EdwardHushaleu <2779522@mail.com>
2020-07-29 20:45:58 +0300

    not work component

--- src/app/shared/components/eventitem/eventitem.component.ts
+++ src/app/shared/components/eventitem/eventitem.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-eventitem',
@@ -6,6 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventitem.component.scss']
 })
 export class EventitemComponent implements OnInit {
+
+@Input() event
+
+colorsTable = {
+  'Кино': "#FF7100",
+  'Концерты': "#E40045",
+  'Фестивали': "#A1A500",
+  'Вечеринки': "#6949D7",
+  'Спектакли': "#009999",
+  'Выставки': "#FF4E40",
+  'Другое': "#35C0CD",
+  'Активности': "#4AA329",
+}
+
+imagesTable={
+  'Кино': 'url(/assets/cinema.jpg)',
+  'Концерты': 'url(/assets/concert.jpg)',
+  'Фестивали': 'url(/assets/festival.jpg)',
+  'Вечеринки': 'url(/assets/party.jpg)',
+  'Спектакли': 'url(/assets/show.jpg)',
+  'Выставки': 'url(/assets/insert.jpg)',
+  'Другое': 'url(/assets/other.jpg)',
+  'Активности': 'url(/assets/active.jpg)'
+};
 
   constructor() { }
 
