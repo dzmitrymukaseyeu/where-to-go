ce59b3aa617a4c4e594f8d1a4f9d1f58c2449b0d
Dzmitry Mukaseyeu <dimamukas@gmail.com>
2020-08-02 00:19:33 +0300

    rewrite if in authguard

--- src/app/shared/guards/auth/auth.guard.ts
+++ src/app/shared/guards/auth/auth.guard.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
 
-    if (localStorage.getItem('userEmail') !== null) {
+    if (localStorage.getItem('userEmail')) {
       return true;
     }
-
+    
     return false;
   }
   
